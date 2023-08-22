d46f265e5ce35fba247b91a79d5ffcca489a7bfa
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-08-21 22:45:20 -0600

    create migration for birds table and foreign key

---
+++ db/migrate/20230822043915_create_bird.rb
@@ -0,0 +1,15 @@
+class CreateBird < ActiveRecord::Migration[7.0]
+  def change
+    create_table :birds do |t|
+      t.integer :flock_id
+      t.string :name
+      t.integer :band_id
+      t.integer :age
+      t.boolean :is_bonded
+
+      t.timestamps
+    end
+
+    add_foreign_key :birds, :flocks, column: :flock_id, primary_key: :id
+  end
+end
